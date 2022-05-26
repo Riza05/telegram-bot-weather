4c988040f858152fc01b36fbd40027f2965a01b6
Riza05 <riza_misrikhanov05@mail.ru>
2022-05-26 15:18:01 +0300

    удаление файла readmi.md

--- src/main/java/com/example/demo/Main/Bot.java
+++ src/main/java/com/example/demo/Main/Bot.java
@@ -2,7 +2,6 @@ package com.example.demo.Main;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
-import org.springframework.stereotype.Controller;
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 import org.telegram.telegrambots.meta.api.objects.Update;
