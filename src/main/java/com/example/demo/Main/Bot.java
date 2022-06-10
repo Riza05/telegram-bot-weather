c3906a75cfc356f595ee5ece52af6d9745a603ff
Riza05 <riza_misrikhanov05@mail.ru>
2022-06-10 17:01:49 +0300

    добавление кнопок и изменение логии при нажатии

--- src/main/java/com/example/demo/Main/Bot.java
+++ src/main/java/com/example/demo/Main/Bot.java
@@ -1,16 +1,13 @@
 package com.example.demo.Main;
 
 import lombok.SneakyThrows;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
-import org.telegram.telegrambots.meta.api.methods.send.SendMediaGroup;
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
-import org.telegram.telegrambots.meta.api.objects.Message;
 import org.telegram.telegrambots.meta.api.objects.Update;
-import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 
-import java.util.ArrayList;
 
 @Component
 public class Bot extends TelegramLongPollingBot {
@@ -18,11 +15,8 @@ public class Bot extends TelegramLongPollingBot {
     private String USER_NAME;
     @Value("${token.bot}")
     private String USER_TOKEN;
-    @Value("${commands.bot}")
-    private String[] COMMANDS;
-    private ArrayList<String> arrayList = new ArrayList<>();
-    private String msg;
-    private Message message;
+    @Autowired
+    private Button button;
 
     @Override
     public String getBotUsername() {
@@ -37,21 +31,31 @@ public class Bot extends TelegramLongPollingBot {
     @SneakyThrows
     @Override
     public void onUpdateReceived(Update update) {
-        if (update.getMessage() != null && update.getMessage().hasText()) {
-            String id = String.valueOf(update.getMessage().getChatId());
+        String message = update.getMessage().getText();
+        String id = String.valueOf(update.getMessage().getChatId());
 
-            if (update.getMessage().getText().equals("/start")) {
-                execute(new SendMessage(id, "Привет,я бот который позволяет узнать погоду.\n" +
-                        "Чтобы начать напиши:/weather\n" +
-                        "Если знать что я умею,напиши:/help\n"));
-            } else if (update.getMessage().getText().equals("/help")) {
-                execute(new SendMessage(id, "Запустить бота:/start\n" +
-                        "Помощь:/help\n" +
-                        "Узнать погоду:/weather"));
-            } else if (update.getMessage().getText().equals("/weather")) {
-                execute(new SendMessage(id, "Напишите город в котором хотите узнать погоду"));
-            } else if (update.getMessage().getText().equals("москва")) {
-                execute(new SendMessage(id, "Погода хорошая"));
+        if (update.getMessage() != null && update.getMessage().hasText()) {
+            if (message.equals("/start")) {
+                execute(button.sendMessageReplyKey(id));
+
+            } else if (message.equals("Помощь")) {
+                execute(new SendMessage(id, "На выбор есть 4 варианта кнопок которые выдают погоду.\n" +
+                        "Чтобы узнать на завтра нажмите:'Погода на сегодня'\n" +
+                        "Чтобы узнать на завтра нажмите:'Погода на завтра'\n" +
+                        "Чтобы узнать на завтра нажмите:'Погода через 5 дней'\n" +
+                        "Чтобы узнать на завтра нажмите:'Погода через 7 дней'"));
+
+            } else if (message.equals("Погода на сегодня")) {
+                execute(new SendMessage(id, "Погода на сегодня"));
+
+            } else if (message.equals("Погода на завтра")) {
+                execute(new SendMessage(id, "Погода на завтра"));
+
+            } else if (message.equals("Погода через 5 дней")) {
+                execute(new SendMessage(id, "Погода через 5 дней"));
+
+            } else if (message.equals("Погода через 7 дней")) {
+                execute(new SendMessage(id, "Погода через 7 дней"));
             }
         }
     }
--- src/main/java/com/example/demo/Main/Button.java
+++ src/main/java/com/example/demo/Main/Button.java
@@ -1,5 +1,66 @@
 package com.example.demo.Main;
 
-public interface Button {
-    String button();
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+import org.telegram.telegrambots.meta.api.objects.replykeyboard.ReplyKeyboardMarkup;
+import org.telegram.telegrambots.meta.api.objects.replykeyboard.buttons.KeyboardButton;
+import org.telegram.telegrambots.meta.api.objects.replykeyboard.buttons.KeyboardRow;
+
+import java.util.ArrayList;
+
+@Component
+public class Button {
+    @Value("${commands.buttons}")
+    private String[] arr;
+
+    public SendMessage sendMessageReplyKey(String chat_id) {
+        ReplyKeyboardMarkup replyKeyboardMarkup = new ReplyKeyboardMarkup();
+        replyKeyboardMarkup.setResizeKeyboard(true);
+        replyKeyboardMarkup.setOneTimeKeyboard(false);
+
+        ArrayList<KeyboardRow> keyboardRows = new ArrayList<>();
+        KeyboardRow keyboardRow1 = new KeyboardRow();
+        keyboardRows.add(keyboardRow1);
+        KeyboardButton btnToday = new KeyboardButton();
+        btnToday.setText("Погода на сегодня");
+        arr[0] = btnToday.getText();
+        keyboardRow1.add(arr[0]);
+
+        KeyboardButton btnTomorrow = new KeyboardButton();
+        btnTomorrow.setText("Погода на завтра");
+        arr[1] = btnTomorrow.getText();
+        keyboardRow1.add(arr[1]);
+
+        KeyboardRow keyboardRow2 = new KeyboardRow();
+        keyboardRows.add(keyboardRow2);
+        KeyboardButton btnFiveDay = new KeyboardButton();
+        btnFiveDay.setText("Погода через 5 дней");
+        arr[2] = btnFiveDay.getText();
+        keyboardRow2.add(arr[2]);
+
+        KeyboardButton btnSevenDay = new KeyboardButton();
+        btnSevenDay.setText("Погода через 7 дней");
+        arr[3] = btnSevenDay.getText();
+        keyboardRow2.add(arr[3]);
+
+        KeyboardRow keyboardRow3 = new KeyboardRow();
+        keyboardRows.add(keyboardRow3);
+        KeyboardButton btnHelp = new KeyboardButton();
+        btnHelp.setText("Помощь");
+        arr[4] = btnHelp.getText();
+        keyboardRow3.add(arr[4]);
+
+        replyKeyboardMarkup.setKeyboard(keyboardRows);
+
+        SendMessage sendMessageRK = new SendMessage();
+        sendMessageRK.setText("Привет! Я бот который показывает погоду.\n" +
+                "Чтобы понять как я работаю советую ознакомиться с инструкцией.\n" +
+                "Чтобы открыть инструкцию нажми на кнопку:'Помощь'");
+        sendMessageRK.setChatId(chat_id);
+
+
+        sendMessageRK.setReplyMarkup(replyKeyboardMarkup);
+        return sendMessageRK;
+    }
 }
